ce4f15d7555e98104bab60f1b2ef23e9e1ac3bb5
Nate Brennand <natebrennand@gmail.com>
2013-10-22 05:51:34 -0400

    add depth first search

--- searches/dfs.py
+++ searches/dfs.py
@@ -1,13 +1,74 @@
 
-def bfs(problem):
-    return
-# function BREADTH-FIRST-SEARCH(problem) returns a solution, or failure
-# node <- a node with STATE = problem.INITIAL-STATE, PATH-COST = 0 if problem.GOAL-TEST(node.STATE) then return SOLUTION(node) frontier <- a FIFO queue with node as the only element
-# explored <- an empty set
-# loop do
-# if EMPTY?(frontier) then return failure
-# node<-POP(frontier) /*choosestheshallowestnodeinfrontier */ add node.STATE to explored
-# for each action in problem.ACTIONS(node.STATE) do
-# child <-CHILD-NODE(problem,node,action)
-# if child.STATE is not in explored or frontier then
-# if problem.GOAL-TEST(child.STATE) then return SOLUTION(child) frontier <-INSERT(child,frontier)
+from copy import deepcopy
+
+def depth_first_search(board):
+    """
+    @param board: a Board obj
+    @return: return iterate_bfs fn call
+    """
+    visited_nodes = set()
+    records = {
+        'node' : 0,
+        'repeat' : 0,
+        'fringe' : 0,
+        'explored' : visited_nodes,
+    }
+    current_queue = [(deepcopy(board), move) for (move, cost) in board.moves_available()]
+
+    print 'repeat\tseen'
+    return iterate_dfs(current_queue, records)
+
+
+def iterate_dfs(queue, records):
+    """
+    @param queue: A queue of boards & their next slated move
+    @param records: a dictionary with various logs that need to be kept
+    """
+
+    while True:
+        print "{}\t{}".format(records['repeat'], len(records['explored']))
+
+        result = dfs(queue, records)
+        if isinstance(result[0], bool) and result[0] == True:
+            return result[1], result[2]     # records & board
+        else:
+            new_queue, records = result
+            records['fringe'] = len(queue)
+
+        queue = new_queue + queue
+
+    print 'failure'
+
+
+def dfs(queue, records):
+    """
+    @param queue: The queue of boards & moves to be evalutated
+    @param records: records to be updated
+
+    @return: new_queue, records: updated versions of each
+    """
+    new_queue = []
+    in_queue = set([])
+
+    # iterates over boards w/ their moves
+    for b, m in queue:
+        records['node'] += 1
+        b.move(m)   # performs move
+        if b.finished():    # checks if done
+            return True, records, b
+
+        next_moves = [move for move,cost in b.moves_available()]
+
+        if next_moves:
+            if hash(b) not in records['explored']:
+                for move in next_moves:
+                    move_hash = hash((hash(b),move))
+                    if move_hash not in in_queue:
+                        new_queue.insert(0, (deepcopy(b), move))
+                        in_queue.add(move_hash)
+                records['explored'].add(hash(b))
+            else:
+                records['repeat'] += 1
+
+    return (new_queue, records)
+
