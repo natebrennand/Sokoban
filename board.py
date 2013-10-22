968df89e9a6c86757cd46782e28bdc575f385818
Nate Brennand <natebrennand@gmail.com>
2013-10-21 23:12:19 -0400

    add documentation

--- board.py
+++ board.py
@@ -23,6 +23,18 @@ from goal   import Goal
 
 
 def load_map(map_str):
+    """
+    @param map_str: A file containing an ascii sokoban puzzle.
+        1st line contains the # of lines.
+            @ : player on ground
+            + : player on goal
+            # : wall
+            $ : box off goal
+            * : box on goal
+    @return: a Board obj representing the map
+
+    Constructs a sokoban board object and returns it.
+    """
     new_board = Board()
     
     map_lines = map_str.split('\n')
@@ -51,6 +63,7 @@ def load_map(map_str):
 class Board(object):
 
     def __init__(self):
+        """ Contructor """
         self.num_lines = 0
         self.walls = {}
         self.goals = {}
@@ -58,6 +71,12 @@ class Board(object):
         self.player = None
 
     def move(self, direction):
+        """
+        @param direction: a direction in the set [u,r,d,l]
+            (assumes this is a valid movement)
+
+        Adjusts the board object given a movement.
+        """
         x_diff, y_diff = DIRECTION[direction]
         pos1 = self.player.x + x_diff, self.player.y + y_diff
         pos2 = self.player.x + 2*x_diff, self.player.y + 2*y_diff
@@ -86,10 +105,11 @@ class Board(object):
             self.player = Player(pos1, 'NORMAL')
 
 
-        return self
-
-
     def finished(self):
+        """
+        Return True if all boxes are on goals.
+        False otherwise.
+        """
         for box in self.boxes.values():
             if box.state == 'OFF':
                 return False
@@ -97,6 +117,11 @@ class Board(object):
 
 
     def moves_available(self):
+        """
+        Returns what moves are available in [u,r,d,l].
+        Checks that moves that involve pushing a block are possible given the
+        placement of walls.
+        """
         x, y = self.player.x, self.player.y
         moves = [('u', (0, -1)), ('r', (1, 0)),
                  ('d', (0, 1)), ('l', (-1, 0))]
@@ -114,27 +139,34 @@ class Board(object):
 
             if not moves:   # no moves available, stop iterating
                 return []
-
         return [move for move, pos in moves]
 
 
     def set_player(self, player):
+        """ sets the player """
         self.player = player
 
     def add_box(self, box):
+        """ adds a box """
         self.boxes[(box.x, box.y)] = box
 
     def add_goal(self, goal):
+        """ adds a goal """
        self.goals[(goal.x, goal.y)] = goal
 
     def add_wall(self, wall):
+        """ adds a wall to the board """
         self.walls[(wall.x, wall.y)] = wall
 
     def deadlock(self):
-        # return True / False
+        unfinished = [b for b in self.boxes.values() if b.status == 'ON']
         pass
 
+
     def __str__(self):
+        """
+        Returns a string representation like the input files
+        """
         str_board = []
         for y in range(self.num_lines):
             str_board.append([' ']*20)
@@ -147,6 +179,4 @@ class Board(object):
 
         str_board = [''.join(line) for line in str_board]
         return '\n'.join(str_board)
-
-
 
