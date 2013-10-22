4acaad27fb948d9e9ac6808a95000a3a7ea0dedd
Nate Brennand <natebrennand@gmail.com>
2013-10-22 04:55:25 -0400

    refactor & comment

--- searches/bfs.py
+++ searches/bfs.py
@@ -1,8 +1,11 @@
 
-from time import sleep
 from copy import deepcopy
 
 def breadth_first_search(board):
+    """
+    @param board: a Board obj
+    @return: return iterate_bfs fn call
+    """
     visited_nodes = set()
     records = {
         'node' : 0,
@@ -12,36 +15,58 @@ def breadth_first_search(board):
     }
     current_queue = [(deepcopy(board), move) for move in board.moves_available()]
 
+    print 'repeat\tseen'
     return iterate_bfs(current_queue, records)
 
 
-def iterate_bfs(current_queue, records):
+def iterate_bfs(queue, records):
+    """
+    @param queue: A queue of boards & their next slated move
+    @param records: a dictionary with various logs that need to be kept
+    """
 
-    result = bfs(current_queue, records)
-    if isinstance(result[0], bool) and result[0] == True:
-        return result[1], result[2]     # records & board
-    else:
-        queue, records = result
-        records['fringe'] = len(queue)
+    while True:
+        print "{}\t{}".format(records['repeat'], len(records['explored']))
 
-    return iterate_bfs(queue, records)
+        result = bfs(queue, records)
+        if isinstance(result[0], bool) and result[0] == True:
+            return result[1], result[2]     # records & board
+        else:
+            queue, records = result
+            records['fringe'] = len(queue)
+
+        if len(queue) == 0:
+            break
+
+    print 'failure'
 
 
 def bfs(queue, records):
-    new_queue = []
+    """
+    @param queue: The queue of boards & moves to be evalutated
+    @param records: records to be updated
 
+    @return: new_queue, records: updated versions of each
+    """
+    new_queue = []
+    in_queue = set([])
+
+    # iterates over boards w/ their moves
     for b, m in queue:
         records['node'] += 1
-        b.move(m)
-
-        if b.finished():
+        b.move(m)   # performs move
+        if b.finished():    # checks if done
             return True, records, b
 
         next_moves = b.moves_available()
+
         if next_moves:
             if hash(b) not in records['explored']:
                 for move in next_moves:
-                    new_queue.append((deepcopy(b), move))
+                    move_hash = hash((hash(b),move))
+                    if move_hash not in in_queue:
+                        new_queue.append((deepcopy(b), move))
+                        in_queue.add(move_hash)
                 records['explored'].add(hash(b))
             else:
                 records['repeat'] += 1
