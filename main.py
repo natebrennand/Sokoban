b1387217e6d339ffc5649b64e6328729627b926c
Nate Brennand <natebrennand@gmail.com>
2013-10-22 03:24:54 -0400

    add report(), run w/ bfs

--- main.py
+++ main.py
@@ -1,25 +1,62 @@
 
 from board import load_map
-from searches import breadth_first_search
+from copy import deepcopy
 import argparse
+from sys import exit
+
+from searches import breadth_first_search as bfs
+searches = [bfs]
 
 
+def report(r, b):
+    print "{} nodes visited".format(r['node'])
+    print "{} nodes repeated".format(r['repeat'])
+    print '{} nodes at the fringe of the search'.format(r['fringe'])
+    print '{} nodes explored'.format(len(r['explored']))
+    print 'Here are the moves made for the solution:'
+    print '\t{}'.format(b.moves)
 
+
+def run_search(board, search):
+    results, board = search(board)
+    report(results, board)
 
 
 def get_args():
     parser = argparse.ArgumentParser(
         description='Get input for the sokoban program.')
-    parser.add_argument('puzzle', metavar='P', type=str, nargs='?',
+    parser.add_argument('puzzle', metavar='P', nargs='?',
                         help='Puzzle to be solved by sokoban')
     parser.add_argument('--test', dest='test', action='store_const',
                         const=True, default=False,
                         help='Run the suite of tests')
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if not args.test and not args.puzzle:
+        print 'Please use a parameter with this program.'
+        print "\tRerun the program with the '--help' flag for more details."
+        exit(1)
+
+    return args
+
+
+def get_map_str(path):
+    map_str = ''
+    with open(path, 'r') as f:
+        map_str = f.read()
+    return map_str
 
 
 if __name__ == '__main__':
     args = get_args()
-    print args.puzzle
-    print args.test
+
+    if args.puzzle:
+        board = load_map(get_map_str(args.puzzle))
+        for search in searches:
+            run_search(deepcopy(board), search)
+
+    elif args.test:
+        print 'running tests...'
+        pass
+
 
